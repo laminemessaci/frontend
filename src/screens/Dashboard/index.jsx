3eb6476754d6001126dff849f275e0121061bce0
laminemessaci <lamine.messaci18@gmail.com>
2022-10-11 09:50:37 +0200

    Add dashboard style

--- src/screens/Dashboard/index.jsx
+++ src/screens/Dashboard/index.jsx
@@ -3,13 +3,16 @@ import { Header } from "../../components/Header/index.jsx";
 import IconComponent from "../../components/IconComponent/index.jsx";
 import Macros from "../../components/Macros/index.jsx";
 import VerticalNavBar from "../../components/VerticalNavBar/index.jsx";
+import { DashboardContainer } from "./index.style.js";
 
 function Dashboard() {
   return (
     <>
       <Header />
-      <Macros userId="12" />
-      <VerticalNavBar />
+      <DashboardContainer>
+        <VerticalNavBar />
+        <Macros userId="12" />
+      </DashboardContainer>
     </>
   );
 }
---
+++ src/screens/Dashboard/index.style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const DashboardContainer = styled.main`
+  display: grid;
+  grid-template-columns: 7.5rem 1fr;
+`;
